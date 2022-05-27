f1da1523a85edc85b030c91f762ac8f8435ba8db
glitterb0mb <srd.dullflame@gmail.com>
2022-05-27 01:16:25 -0400

    add pic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">ayyy lmao</header>
+      <header className="App-header">
+        <img
+          src="https://cdn.dribbble.com/users/788099/screenshots/16734162/media/25c35cfff9374783ad60cbf6b4fb646d.jpg?compress=1&resize=400x300&vertical=top"
+          alt="girlcoding"
+        ></img>
+      </header>
     </div>
   );
 }
