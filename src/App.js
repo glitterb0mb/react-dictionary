7057803c8e8a506709681c090ce2f7729f1cf38e
glitterb0mb <srd.dullflame@gmail.com>
2022-05-27 21:03:51 -0400

    setup search function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src="https://cdn.dribbble.com/users/788099/screenshots/16734162/media/25c35cfff9374783ad60cbf6b4fb646d.jpg?compress=1&resize=400x300&vertical=top"
-          alt="girlcoding"
-        ></img>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src="https://cdn.dribbble.com/users/788099/screenshots/16734162/media/25c35cfff9374783ad60cbf6b4fb646d.jpg?compress=1&resize=400x300&vertical=top"
+            alt="girlcoding"
+          ></img>
+        </header>
+        <Dictionary />
+        <div className="App-footer">Coded by Sarah Davis</div>
+      </div>
     </div>
   );
 }
