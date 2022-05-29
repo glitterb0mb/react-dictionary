112c58075117292fbc6ec885d09ae0271468972e
glitterb0mb <srd.dullflame@gmail.com>
2022-05-29 02:30:56 -0400

    changed pic

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <div className="container">
         <header className="App-header">
           <img
-            src="https://cdn.dribbble.com/users/788099/screenshots/16734162/media/25c35cfff9374783ad60cbf6b4fb646d.jpg?compress=1&resize=400x300&vertical=top"
-            alt="girlcoding"
+            src="https://miro.medium.com/max/1400/0*-ATQ3-hGyZxfZqmo"
+            alt="dicpic"
           ></img>
         </header>
         <Dictionary />
