d6849be96b273002e887a793d05ba88a2886e0ae
glitterb0mb <srd.dullflame@gmail.com>
2022-05-29 02:18:14 -0400

    search now live

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [results, setResults] = useState(null);
   let [keyword, setKeyword] = useState("");
-  function handleResponse(response) {}
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
   function handleKeyword(event) {
     setKeyword(event.target.value);
   }
@@ -19,6 +23,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeyword} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
