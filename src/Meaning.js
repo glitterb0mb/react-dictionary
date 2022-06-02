a5d5767090ed9c60fdf7402ee614dfdbbef0eaa4
glitterb0mb <srd.dullflame@gmail.com>
2022-06-01 23:23:57 -0400

    add synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,9 +8,19 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
+            <p>
+              <strong>Definition:</strong>
+              {""}
+              {definition.definition}
+            </p>
             <br />
-            <em>{definition.example}</em>
+            <em>
+              <strong>Example: </strong>
+              {""}
+              {definition.example}
+            </em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
